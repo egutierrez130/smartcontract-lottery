cadb4d85119e944e854eae86cc5cd5cefe51ec4b
Edgar Gutierrez <edgargutierrez130@gmail.com>
2023-04-30 23:56:49 -0700

    Added and Modified Hardhat Config and .env File

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,12 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    // const chainId = network.config.chainId
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations,
+    })
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,32 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6
+        }
+    }
     solidity: "0.8.7",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
 }
