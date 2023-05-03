1357c085787a564a85423739ba0988fde4ff120d
Edgar Gutierrez <edgargutierrez130@gmail.com>
2023-05-02 20:39:03 -0700

    Added 3 new tests to the enterRaffle function

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -1,18 +1,19 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Raffle Unit Tests", function () {
-          let raffle, vrfCoordinatorV2Mock
+          let raffle, vrfCoordinatorV2Mock, raffleEntranceFee, deployer
           const chainId = network.config.chainId
 
           beforeEach(async () => {
-              const { deployer } = await getNamedAccounts()
+              deployer = (await getNamedAccounts()).deployer
               await deployments.fixture(["all"])
               raffle = await ethers.getContract("Raffle", deployer)
               vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+              raffleEntranceFee = await raffle.getEntranceFee()
           })
 
           describe("constructor", function () {
@@ -22,6 +23,24 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   const interval = await raffle.getInterval()
                   assert.equal(raffleState.toString(), "0")
                   assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+              })
+          })
+          describe("enterRaffle", function () {
+              it("reverts when you don't pay enough", async () => {
+                  await expect(raffle.enterRaffle()).to.be.revertedWith(
+                      "Raffle__NotEnoughETHEntered"
+                  )
+              })
+              it("records players when they enter", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  const playerFromContract = await raffle.getPlayer(0)
+                  assert.equal(playerFromContract, deployer)
+              })
+              it("emits event on enter", async () => {
+                  await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.emit(
+                      raffle,
+                      "RaffleEnter"
+                  )
               })
           })
       })
