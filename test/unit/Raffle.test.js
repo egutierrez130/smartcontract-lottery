54bd55784f965da9586c5e784f4595ce3840c66b
Edgar Gutierrez <edgargutierrez130@gmail.com>
2023-05-04 23:18:47 -0700

    Completed All performUpkeep tests

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -100,5 +100,16 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                       "Raffle__UpkeepNotNeeded"
                   )
               })
+              it("updates the raffle state, emits and event, and calls the vrf coordinator", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                  await network.provider.send("evm_mine", [])
+                  const txResponse = await raffle.performUpkeep([])
+                  const txReceipt = await txResponse.wait(1)
+                  const requestId = txReceipt.events[1].args.requestId
+                  const raffleState = await raffle.getRaffleState()
+                  assert(requestId.toNumber() > 0)
+                  assert(raffleState.toString() == "1")
+              })
           })
       })
